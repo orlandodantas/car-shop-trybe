bbbe30e70541308765b887c12bc6cbb7d7b3e8bd
root <root@pop-os.localdomain>
2022-08-20 18:44:34 -0300

    refactor: Clear code

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,5 @@
 import App from './app';
-// import handleError from './middleware/handleError';
-// import CarRouter from './routes/carRouter';
 
 const server = new App();
 
-// server.addRouter('/cars', CarRouter);
-
-// server.app.use(handleError);
 export default server;
